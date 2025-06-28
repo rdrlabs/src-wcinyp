7d19b66cd7fc3e23831fddff074c0dc53d8374f4
ray <zenith.trail6417@eagereverest.com>
2025-06-28 13:21:23 +0000

    Remove the default Docusaurus SVG images

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -5,8 +5,8 @@ import type * as Preset from '@docusaurus/preset-classic';
 // This runs in Node.js - Don't use client-side code here (browser APIs, JSX...)
 
 const config: Config = {
-  title: 'My Site',
-  tagline: 'Dinosaurs are cool',
+  title: 'WCINYP Resource Hub',
+  tagline: 'Weill Cornell Imaging at NewYork-Presbyterian Resources',
   favicon: 'img/favicon.ico',
 
   // Future flags, see https://docusaurus.io/docs/api/docusaurus-config#future
@@ -15,21 +15,21 @@ const config: Config = {
   },
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://wcinyp-resources.netlify.app',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'rdrlabs', // Usually your GitHub org/user name.
+  projectName: 'src-wcinyp', // Usually your repo name.
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
 
   // Even if you don't use internationalization, you can use this field to set
-  // useful metadata like html lang. For example, if your site is Chinese, you
+  // useful metadata like html lang. For example, if your site is Chinese,
   // may want to replace "en" with "zh-Hans".
   i18n: {
     defaultLocale: 'en',
@@ -45,23 +45,9 @@ const config: Config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+            'https://github.com/rdrlabs/src-wcinyp/tree/main/',
         },
-        blog: {
-          showReadingTime: true,
-          feedOptions: {
-            type: ['rss', 'atom'],
-            xslt: true,
-          },
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
-          editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
-          // Useful options to enforce blogging best practices
-          onInlineTags: 'warn',
-          onInlineAuthors: 'warn',
-          onUntruncatedBlogPosts: 'warn',
-        },
+        blog: false, // Disable blog feature
         theme: {
           customCss: './src/css/custom.css',
         },
@@ -73,9 +59,9 @@ const config: Config = {
     // Replace with your project's social card
     image: 'img/docusaurus-social-card.jpg',
     navbar: {
-      title: 'My Site',
+      title: 'WCINYP Resources',
       logo: {
-        alt: 'My Site Logo',
+        alt: 'WCINYP Logo',
         src: 'img/logo.svg',
       },
       items: [
@@ -83,11 +69,16 @@ const config: Config = {
           type: 'docSidebar',
           sidebarId: 'tutorialSidebar',
           position: 'left',
-          label: 'Tutorial',
+          label: 'Resources',
         },
-        {to: '/blog', label: 'Blog', position: 'left'},
         {
-          href: 'https://github.com/facebook/docusaurus',
+          href: '/docs/emergency',
+          position: 'left',
+          label: '🚨 Emergency',
+          className: 'navbar-emergency-link',
+        },
+        {
+          href: 'https://github.com/rdrlabs/src-wcinyp',
           label: 'GitHub',
           position: 'right',
         },
@@ -97,46 +88,54 @@ const config: Config = {
       style: 'dark',
       links: [
         {
-          title: 'Docs',
+          title: 'Quick Links',
           items: [
             {
-              label: 'Tutorial',
-              to: '/docs/intro',
+              label: 'MRI Manual',
+              to: '/docs/mri-manual',
+            },
+            {
+              label: 'Contact Directory',
+              to: '/docs/contacts',
+            },
+            {
+              label: 'Emergency Procedures',
+              to: '/docs/emergency',
             },
           ],
         },
         {
-          title: 'Community',
+          title: 'Departments',
           items: [
             {
-              label: 'Stack Overflow',
-              href: 'https://stackoverflow.com/questions/tagged/docusaurus',
+              label: 'Radiology',
+              href: '#',
             },
             {
-              label: 'Discord',
-              href: 'https://discordapp.com/invite/docusaurus',
+              label: 'Breast Imaging',
+              href: '#',
             },
             {
-              label: 'X',
-              href: 'https://x.com/docusaurus',
+              label: 'Administration',
+              href: '#',
             },
           ],
         },
         {
-          title: 'More',
+          title: 'Resources',
           items: [
             {
-              label: 'Blog',
-              to: '/blog',
+              label: 'Scheduling Guidelines',
+              to: '/docs/scheduling',
             },
             {
-              label: 'GitHub',
-              href: 'https://github.com/facebook/docusaurus',
+              label: 'Safety Protocols',
+              to: '/docs/safety',
             },
           ],
         },
       ],
-      copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+      copyright: `Copyright © ${new Date().getFullYear()} Weill Cornell Imaging at NewYork-Presbyterian.`,
     },
     prism: {
       theme: prismThemes.github,
